698172356230dccae275f2a783249a3dc45e3f66
Hassan Ayub <pcplayer801@gmail.com>
2026-06-08 10:37:56 +0500

    Initial commit

--- src/components/sections/ScrollVideo.tsx
+++ src/components/sections/ScrollVideo.tsx
@@ -52,7 +52,7 @@ export default function ScrollVideo() {
               loop
               playsInline
             >
-              <source src="https://drive.google.com/file/d/1dz_raIdB4m2tcOqBgO1VC_Fd-REqA2aR/view?usp=drive_link" type="video/mp4" />
+            <source src="/202606010833 (1).zip" type="video/mp4" />
             </video>
           </motion.div>
         </div>
